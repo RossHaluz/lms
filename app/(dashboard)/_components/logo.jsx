5ae862cc4c973a658cecb4c5ca10f66ce45c79dd
Rostyslav Haluzinskiy <106879174+RossHaluz@users.noreply.github.com>
2024-03-12 23:44:35 +0200

    add

--- app/(dashboard)/_components/logo.jsx
+++ app/(dashboard)/_components/logo.jsx
@@ -1,16 +1,7 @@
 import Image from "next/image";
 
 const Logo = () => {
-  return (
-    <Image
-      height={130}
-      width={130}
-      className="w-[130px] h-[130px]"
-      alt="Logo"
-      src="/logo.svg"
-      priority
-    />
-  );
+  return <Image height={130} width={130} alt="Logo" src="/logo.svg" priority />;
 };
 
 export default Logo;
